fa181faa3f99a35c7f0ae33a20bd72b80399a578
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-18 20:09:41 +0700

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -23,11 +23,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"**Judul:** `{results[i]['title']}`\n"
-            text += f"**Durasi:** {results[i]['duration']}\n"
-            text += f"**Views:** {results[i]['views']}\n"
-            text += f"**Channel:** {results[i]['channel']}\n"
-            text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"🏷️ **Judul:** `{results[i]['title']}`\n"
+            text += f"⏱️ **Durasi:** {results[i]['duration']}\n"
+            text += f"👁️‍🗨️ **Views:** {results[i]['views']}\n"
+            text += f"💡 **Channel:** {results[i]['channel']}\n"
+            text += f"🖇️ https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
