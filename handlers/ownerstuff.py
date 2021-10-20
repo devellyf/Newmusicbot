019196138801671f02151e63eea9a4ceed6c3380
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-21 01:05:04 +0700

    Update ownerstuff.py

--- handlers/ownerstuff.py
+++ handlers/ownerstuff.py
@@ -49,12 +49,131 @@ from config import (
     HEROKU_URL,
     U_BRANCH,
     UPSTREAM_REPO,
+    OWNER_ID,
     BOT_NAME,
 )
+from helpers.database import db
+from helpers.dbtools import main_broadcast_handler
 from handlers.song import get_text, humanbytes
 from helpers.filters import command
 from helpers.decorators import sudo_users_only
 
+
+# Stats Of Your Bot
+@Client.on_message(command("stats"))
+@sudo_users_only
+async def botstats(_, message: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+    total_users = await db.total_users_count()
+    await message.reply_text(
+        text=f"**📊 stats of [{BOT_NAME}](https://t.me/{BOT_USERNAME})** \n\n**🤖 bot version:** `{__version__}` \n\n**🙎🏼 total users:** \n » **on bot pm:** `{total_users}` \n\n**💾 disk usage:** \n » **disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage:** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
+        disable_web_page_preview=True,
+        parse_mode="Markdown",
+        quote=True,
+    )
+
+
+@Client.on_message(
+    filters.private
+    & filters.command("broadcast")
+    & filters.user(OWNER_ID)
+    & filters.reply
+)
+async def broadcast_handler_open(_, m: Message):
+    await main_broadcast_handler(m, db)
+
+
+@Client.on_message(filters.private & filters.command("block"))
+@sudo_users_only
+async def ban(c: Client, m: Message):
+    if len(m.command) == 1:
+        await m.reply_text(
+            "» this command for ban user from using your bot, read /help for more info !",
+            quote=True,
+        )
+        return
+    try:
+        user_id = int(m.command[1])
+        ban_duration = m.command[2]
+        ban_reason = " ".join(m.command[3:])
+        ban_log_text = f"🚷 **banned user !** \n\nuser id: `{user_id}` \nduration: `{ban_duration}` \nreason: `{ban_reason}`"
+        try:
+            await c.send_message(
+                user_id,
+                f"😕 sorry, you're banned!** \n\nreason: `{ban_reason}` \nduration: `{ban_duration}` day(s). \n\n**💬 message from owner: ask in @{GROUP_SUPPORT} if you think this was an mistake.",
+            )
+            ban_log_text += "\n\n✅ this notification was sent to that user"
+        except:
+            traceback.print_exc()
+            ban_log_text += f"\n\n❌ **failed sent this notification to that user** \n\n`{traceback.format_exc()}`"
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        print(ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+    except:
+        traceback.print_exc()
+        await m.reply_text(
+            f"❌ an error occoured, traceback is given below:\n\n`{traceback.format_exc()}`",
+            quote=True,
+        )
+
+
+# Unblock User
+@Client.on_message(filters.private & filters.command("unblock"))
+@sudo_users_only
+async def unban(c: Client, m: Message):
+    if len(m.command) == 1:
+        await m.reply_text(
+            "» this command for unban user, read /help for more info !", quote=True
+        )
+        return
+    try:
+        user_id = int(m.command[1])
+        unban_log_text = f"🆓 **unbanned user !** \n\n**user id:**{user_id}"
+        try:
+            await c.send_message(user_id, "🎊 congratulations, you was unbanned!")
+            unban_log_text += "\n\n✅ this notification was sent to that user"
+        except:
+            traceback.print_exc()
+            unban_log_text += f"\n\n❌ **failed sent this notification to that user** \n\n`{traceback.format_exc()}`"
+        await db.remove_ban(user_id)
+        print(unban_log_text)
+        await m.reply_text(unban_log_text, quote=True)
+    except:
+        traceback.print_exc()
+        await m.reply_text(
+            f"❌ an error occoured, traceback is given below:\n\n`{traceback.format_exc()}`",
+            quote=True,
+        )
+
+
+# Blocked User List
+@Client.on_message(filters.private & filters.command("blocklist"))
+@sudo_users_only
+async def _banned_usrs(_, m: Message):
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ""
+    async for banned_user in all_banned_users:
+        user_id = banned_user["id"]
+        ban_duration = banned_user["ban_status"]["ban_duration"]
+        banned_on = banned_user["ban_status"]["banned_on"]
+        ban_reason = banned_user["ban_status"]["ban_reason"]
+        banned_usr_count += 1
+        text += f"🆔 **user id**: `{user_id}`\n⏱ **duration**: `{ban_duration}`\n🗓 **banned date**: `{banned_on}`\n💬 **reason**: `{ban_reason}`\n\n"
+    reply_text = f"🚷 **total banned:** `{banned_usr_count}`\n\n{text}"
+    if len(reply_text) > 4096:
+        with open("banned-user-list.txt", "w") as f:
+            f.write(reply_text)
+        await m.reply_document("banned-user-list.txt", True)
+        os.remove("banned-user-list.txt")
+        return
+    await m.reply_text(reply_text, True)
 
 # ====== UPDATER ======
 
@@ -75,8 +194,7 @@ async def edit_or_reply(msg: Message, **kwargs):
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-@Client.on_message(command("update"))
-@sudo_users_only
+@Client.on_message(command("update") & filters.user(OWNER_ID))
 async def updatebot(_, message: Message):
     msg = await message.reply_text("**updating bot, please wait for a while...**")
     try:
@@ -190,8 +308,7 @@ async def logswen(client: Client, message: Message, happ):
 
 
 # Restart Bot
-@Client.on_message(command("restart"))
-@sudo_users_only
+@Client.on_message(command("restart") & filters.user(OWNER_ID))
 @_check_heroku
 async def restart(client: Client, message: Message, hap):
     await message.reply_text("`restarting now, please wait...`")
@@ -199,8 +316,7 @@ async def restart(client: Client, message: Message, hap):
 
 
 # Set Heroku Var
-@Client.on_message(command("setvar"))
-@sudo_users_only
+@Client.on_message(command("setvar") & filters.user(OWNER_ID))
 @_check_heroku
 async def setvar(client: Client, message: Message, app_):
     msg = await message.reply_text("`please wait...`")
@@ -222,8 +338,7 @@ async def setvar(client: Client, message: Message, app_):
 
 
 # Delete Heroku Var
-@Client.on_message(command("delvar"))
-@sudo_users_only
+@Client.on_message(command("delvar") & filters.user(OWNER_ID))
 @_check_heroku
 async def delvar(client: Client, message: Message, app_):
     msg = await message.reply_text("`please wait...!`")
