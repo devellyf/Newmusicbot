b4191d1115fa560d44644413c7ab0c0d360e2979
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-29 02:12:09 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -452,6 +452,7 @@ async def m_cb(b, cb):
                 await cb.message.edit(mmk, reply_markup=keyboard)
 
     elif type_ == "leave":
+        kntls = "✅ __The Userbot has disconnected from voice chat__"
         if chet_id in callsmusic.pytgcalls.active_calls:
             try:
                 callsmusic.queues.clear(chet_id)
@@ -459,8 +460,12 @@ async def m_cb(b, cb):
                 pass
 
             callsmusic.pytgcalls.leave_group_call(chet_id)
-            await b.send_message(chid, "✅ __The Userbot has disconnected from voice chat__")
-            await cb.message.delete()
+            await cb.message.edit(
+                    kntlss,
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton("🗑 Close", callback_data="closed")]]
+                    ),
+                )
         else:
             await cb.answer(
                 "assistant is not connected to voice chat !", show_alert=True
