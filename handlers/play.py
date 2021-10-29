9daba5f82fe6909e9d6d073d40d2c123803437b2
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-29 18:50:18 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -572,7 +572,7 @@ async def play(_, message: Message):
             ]
         )
         file_name = get_file_name(audio)
-        title = file_name
+        title = "Telegram audio"
         thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
         thumbnail = thumb_name
         ctitle = message.chat.title
@@ -726,7 +726,7 @@ async def play(_, message: Message):
         await lel.delete()
         await _.send_photo(chid,
             photo="final.png",
-            caption=f"🏷 **Name:** [Telegram audio]({url})\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {message.from_user.mention}\n\n🔢 Track position » `{position}`",
+            caption=f"🏷 **Name:** [{title}]({url})\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {message.from_user.mention}\n\n🔢 Track position » `{position}`",
             reply_markup=keyboard
         )
     else:
@@ -747,7 +747,7 @@ async def play(_, message: Message):
         await lel.delete()
         await _.send_photo(chid,
             photo="final.png",
-            caption = f"🏷 **Name:** [Telegram audio]({url})\n⏱ **duration:** `{duration}`\n💡 **Status:** `Playing`\n" \
+            caption = f"🏷 **Name:** [{title}]({url})\n⏱ **duration:** `{duration}`\n💡 **Status:** `Playing`\n" \
                     + f"🎧 **Request by:** {r_by.mention} \n",
             reply_markup=keyboard
         )
