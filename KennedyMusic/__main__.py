bc15fa17dd2c17baf036f53d651551c041306eac
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-30 03:00:25 +0700

    au dah kontol

--- KennedyMusic/__main__.py
+++ KennedyMusic/__main__.py
@@ -1,23 +1,21 @@
 import requests
-from pyrogram import Client as Bot
-
-from callsmusic.callsmusic import run
+from pyrogram import Client
 from KennedyMusic.config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
+from KennedyMusic.callsmusic.callsmusic import run
+
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
     file.write(response.content)
 
 
-bot = Bot(
+bot = Client(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="KennedyMusic/handlers")
 )
-
-print("[INFO]: YOUR MUSIC BOT STARTED!")
 
 bot.start()
 run()
