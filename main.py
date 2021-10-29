25c42dc0a42b49900be942c1d7f27ba48dfb86ac
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-10-30 01:48:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from pyrogram import Client as Bot
 
-from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
+from KennedyMusic.callsmusic import run
+from KennedyMusic.config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
