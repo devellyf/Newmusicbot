3cb4f2c8b3e408c4105a16ff26f83996493deab7
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-11-08 23:38:34 +0700

    Update database.py

--- KennedyMusic/helpers/database.py
+++ KennedyMusic/helpers/database.py
@@ -95,8 +95,61 @@ async def clean_restart_stage() -> dict:
     return {"chat_id": data["chat_id"], "message_id": data["message_id"]}
 
 
+## Queue Chats
+
+async def get_active_chats() -> list:
+    chats = pytgdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
+    
+async def is_active_chat(chat_id: int) -> bool:
+    chat = await pytgdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+async def add_active_chat(chat_id: int):
+    is_served = await is_active_chat(chat_id)
+    if is_served:
+        return
+    return await pytgdb.insert_one({"chat_id": chat_id})
+
+async def remove_active_chat(chat_id: int):
+    is_served = await is_active_chat(chat_id)
+    if not is_served:
+        return
+    return await pytgdb.delete_one({"chat_id": chat_id})
+
+  
+## Music Playing or Paused  
+    
+async def is_music_playing(chat_id: int) -> bool:
+    chat = await admindb.find_one({"chat_id_toggle": chat_id})
+    if not chat:
+        return True
+    return False
+
+async def music_on(chat_id: int):
+    dis_kontol = await is_music_playing(chat_id)
+    if dis_kontol:
+        return
+    return await admindb.delete_one({"chat_id_toggle": chat_id})
+
+async def music_off(chat_id: int):
+    dis_kontol = await is_music_playing(chat_id)
+    if not dis_kontol:
+        return
+    return await admindb.insert_one({"chat_id_toggle": chat_id})
+
+
 # Database
 db = Database(DATABASE_URL, BOT_USERNAME)
 mongo_db_lmao = MongoClient(DATABASE_URL)
 dcmdb = mongo_db_lmao.handlers
 restart_stagedb = mongo_db_lmao.restart_stage
+pytgdb = mongo_db_lmao.pytg
+admindb = mongo_db_lmao.admin
