22a3fd96c69bd2ada15b7a17d1e7f2a50e4ca3cd
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-11-08 21:39:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pytgcalls import idle
 from KennedyMusic.callsmusic import run
 from pyrogram import Client as Bot
+from KennedyMusic.helpers.database import clean_restart_stage
 from KennedyMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 
 
@@ -16,9 +17,22 @@ bot = Bot(
     API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="KennedyMusic/handlers"),
-)
+).start()
 
 
-bot.start()
+async def load_start():
+    restart_data = await clean_restart_stage()
+    if restart_data:
+        print("[INFO]: SENDING RESTART STATUS")
+        try:
+            await app.edit_message_text(
+                restart_data["chat_id"],
+                restart_data["message_id"],
+                "**Restarted the Bot Successfully.**",
+            )
+        except Exception:
+            pass
+        await bot.send_message(LOG_CHANNEL, "✅ KennedyMusic Is Now Online")
+       
 run()
 idle()
