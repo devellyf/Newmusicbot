e56b6ccde3137c78881a2944566840ecd2dce1a1
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-11-09 00:09:47 +0700

    Update admins.py

--- KennedyMusic/handlers/admins.py
+++ KennedyMusic/handlers/admins.py
@@ -6,7 +6,7 @@ from KennedyMusic.config import BOT_USERNAME, que
 from KennedyMusic.cache.admins import admins
 from KennedyMusic.handlers.play import cb_admin_check
 from KennedyMusic.helpers.channelmusic import get_chat_id
-from KennedyMusic.helpers.dbtools import delcmd_is_on, delcmd_off, delcmd_on, handle_user_status
+from KennedyMusic.helpers.dbtools import delcmd_is_on, delcmd_off, delcmd_on
 from KennedyMusic.helpers.decorators import authorized_users_only, errors
 from KennedyMusic.helpers.filters import command, other_filters
 from pyrogram import Client, filters
