36cb6e9a6180aa7aca540874364dd5cd0315b826
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-11-12 22:11:45 +0700

    Update youtube.py

--- KennedyMusic/downloaders/youtube.py
+++ KennedyMusic/downloaders/youtube.py
@@ -5,6 +5,7 @@ from yt_dlp import YoutubeDL
 from KennedyMusic.config import DURATION_LIMIT
 from KennedyMusic.helpers.errors import DurationLimitError
 
+
 ydl_opts = {
     "format": "bestaudio/best",
     "geo-bypass": True,
@@ -19,12 +20,8 @@ def download(url: str) -> str:
     duration = round(info["duration"] / 60)
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
         )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)",
-        )
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
